c99e7d0a25f499d62b6309312f0ca0461fc87ec4
SabaMuladze <muladze66@bk.ru>
2023-10-10 22:32:53 +0400

    Done

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div class="w-[50%] h-[50vh] bg-white flex items-center justify-center rounded-lg">
+        <h1 class=" max-sm:text-2xl max-md:text-4xl text-6xl text-center" v-if="name == null">You did not enter your name
+        </h1>
+        <h1 class=" max-sm:text-2xl max-md:text-4xl text-6xl text-center" v-else-if="name.length > 0">Hello : {{ name }}!
+        </h1>
+        <h1 class=" max-sm:text-2xl max-md:text-4xl text-6xl text-center" v-else>You did not enter your name</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: prompt("Please enter your name:"),
+        }
+    },
+}
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+</style>
